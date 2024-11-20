cf417e95db6eebdfbec9e534e1a88e5437047237
Diego-Goncalves-Ferreira <88411595+DiegoMcall@users.noreply.github.com>
2024-11-20 16:57:43 -0300

    Adicionei a tela de contatos

--- meet_point/App.js
+++ meet_point/App.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import { View } from 'react-native';
 import Primeira from './telas/primeira';
 import Lgrupos from './telas/Lgrupos';
+import Lcontatos from './telas/Lcontatos';
 
 export default function App() {
 return(
-    <Lgrupos />
+    <Lcontatos />
 );
 }
